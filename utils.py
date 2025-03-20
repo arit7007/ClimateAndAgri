48a19bd494b00cfdf18847f91c377f693144d36a
arit7007 <126838437+arit7007@users.noreply.github.com>
2025-03-19 22:30:48 -0700

    Create utils.py
    commonly used modules used by other programs

---
+++ utils.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+def save_df(df: pd.DataFrame, file_name):
+    extn = os.path.splitext(file_name)[1]
+
+    if extn == ".csv":
+        df.to_csv(file_name, index=False)
+    elif extn == '.parquet':
+        df.to_parquet(file_name)
+    else:
+        logging.error(f"Invalid file extension. Only .csv or .parquet are recognized")
+
+    logging.info(f"File is saved at {file_name}")

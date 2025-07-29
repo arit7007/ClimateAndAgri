18b449e5043d11d20ff92d8158259a637ebdc029
arit7007 <126838437+arit7007@users.noreply.github.com>
2025-07-28 23:13:42 -0700

    Update download_climate_data.py

--- download_climate_data.py
+++ download_climate_data.py
@@ -207,7 +207,7 @@ def climate_data_pipeline(states: List[str], start_year: int, end_year: int):
     logging.info("Aggregating to county-year...")
     grow_months = list(range(4, 10))  # Aggregating for the common growing season, without considering for crop-specific growing seasons
     county_df = aggregate_climate_by_period(joined_df, months=grow_months)
-    save_df(county_df, DATA_PATH / f"climate_with_county_aggregated_{start_year}_{end_year}.csv")
+    save_df(county_df, DATA_PATH / f"final_climate_data_{start_year}_{end_year}.csv")
 
     logging.info("Pipeline complete!")
 

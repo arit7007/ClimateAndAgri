0060674830e705006bc32d37b32cfd10ed406e0a
arit7007 <126838437+arit7007@users.noreply.github.com>
2025-07-29 21:01:08 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,12 @@
-# Keeps the configuration parameters used across programs
-
 import os
 import logging
 from dotenv import load_dotenv
+from pathlib import Path
 
-DATA_PATH = "../data/"
-CLIMATE_DATA = DATA_PATH + "climate_data.csv"
-CLIMATE_DATA_WITH_FIPS = DATA_PATH + "climate_data_with_fips.csv"
-CROP_YIELD_DATA = DATA_PATH + "crop_yield_data.csv"
-CLIMATE_YIELD_DATA = DATA_PATH + "climate_and_yield_data.csv"
+DATA_PATH = Path("../data")
+COUNTY_SHAPEFILE = "https://www2.census.gov/geo/tiger/GENZ2022/shp/cb_2022_us_county_500k.zip"
 
-logging.basicConfig(level=logging.WARN, format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # API Endpoints
 NASS_BASE_URL = "https://quickstats.nass.usda.gov/api/api_GET/"
@@ -20,7 +16,9 @@ load_dotenv()
 # API Keys
 NASS_API_KEY = os.getenv("NASS_API_KEY")
 NOAA_TOKEN = os.getenv("NOAA_TOKEN")
+SH_CLIENT_ID = os.getenv("SH_CLIENT_ID")
+SH_CLIENT_SECRET = os.getenv("SH_CLIENT_SECRET")
+GEE_PROJECT_ID=os.getenv("GEE_PROJECT_ID")
 
 #logging.info(f"Loaded USDA API Key: {NASS_API_KEY[:5]}*****" if NASS_API_KEY else "USDA NASS API Key is not set")
 #logging.info(f"Loaded NOAA API Key: {NOAA_TOKEN[:5]}*****" if NOAA_TOKEN else "NOAA Token is not set")
-
